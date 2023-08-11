af4cf802c24275aa15e3b3eb29f2f262b1d54cd7
yukipeti <ugachan0817@gmail.com>
2023-08-12 02:18:13 +0900

    環境整備

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+/** @format */
+
+module.exports = {
+    apps: [
+        {
+            name: "StudyRoom-BOT",
+            script: "./main.js",
+            instance_var: "INSTANCE_ID",
+            env_dev: {
+                NODE_ENV: "development"
+            },
+            env_stg: {
+                NODE_ENV: "staging"
+            },
+            env_prod: {
+                NODE_ENV: "production"
+            },
+            time: true,
+            watch: ["./src/"],
+            ignore_watch: ["./src/channel-target.json"],
+        },
+    ],
+};
