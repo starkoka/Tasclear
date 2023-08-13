c4e339998c165a2a90ca243336676eafa361588c
starkoka <j22437@inc.kisarazu.ac.jp>
2023-08-13 19:44:23 +0900

    getUser関数多分できた(2回目)

--- functions/userData.js
+++ functions/userData.js
@@ -1,7 +1,71 @@
 const {StringSelectMenuBuilder, StringSelectMenuOptionBuilder, EmbedBuilder, ActionRowBuilder} = require("discord.js");
 const system = require('./logsystem.js');
 const db = require('./db.js');
+const userData = require('./userData.js');
 require('date-utils');
+
+const ZERO = [0,0,0,0,0,0,0];
+
+/**
+ * userIdから情報を取得し、相対的な日付のデータを修正して返す&更新する関数
+ * @param userId 取得するユーザーID
+ * @param userId
+ * @returns {Promise<WithId<Document>>}
+ */
+exports.getUser = async function func(userId){
+    const user = (await db.find("main","user",{userId:userId}))[0];
+    const date = new Date();
+    const nextMonday = new Date();
+    if(user.lastUpdate.getDay() === 0){
+        nextMonday.setDate(user.lastUpdate.getDate() + 1);
+    }
+    else{
+        nextMonday.setDate(user.lastUpdate.getDate() - user.lastUpdate.getDay() + 7 + 1);
+    }
+
+
+    if(date.toFormat("YYYYMMDD") !== user.lastUpdate.toFormat("YYYYMMDD")) {　//同じ日
+        if (nextMonday > date) { //同じ週
+            for (let i = 0; i < (date.getDay()===0 ? 7 : date.getDay()) ; i++) {
+                if (user.weeklyData[i] === null) user.weeklyData[i] = 0;
+            }
+        }
+        else { //異なる週
+            let i=1;
+            for(; i < 1+4; i++){
+                nextMonday.setDate(nextMonday.getDate() + 7);
+                if (nextMonday > date)break;
+            }
+            
+            switch(i){
+                case 1:
+                    user.monthlyData = [user.weeklyData,user.monthlyData[0],user.monthlyData[1]];
+                    break;
+                case 2:
+                    user.monthlyData = [ZERO,user.weeklyData,user.monthlyData[0]];
+                    break;
+                case 3:
+                    user.monthlyData = [ZERO,ZERO,user.weeklyData];
+                    break;
+                default:
+                    user.monthlyData = [ZERO,ZERO,ZERO];
+                    break;
+            }
+            user.weeklyData=[];
+            const loop = (date.getDay() === 0 ? 7 : date.getDay());
+            for(let i = 0; i < loop; i++){
+                user.weeklyData.push(0);
+            }
+            for(let i = 0; i < 7-loop; i++){
+                user.weeklyData.push(null);
+            }
+        }
+        user.lastUpdate = date;
+        await db.update("main","user",{"userId":userId},{$set:user});
+    }
+    delete user._id;
+    return user;
+}
 
 /***
  * 空のuserDataデータを作成する(既にある場合は実行されない)
@@ -14,10 +78,11 @@ exports.makeUserData = async function func(userId){
         const date = new Date();
 
         const weeklyData=[];
-        for(let i = 0; i < date.getDay()+1; i++){
+        const loop = (date.getDay() === 0 ? 7 : date.getDay());
+        for(let i = 0; i < loop; i++){
             weeklyData.push(0);
         }
-        for(let i = 0; i < 7-(date.getDay()+1); i++){
+        for(let i = 0; i < 7-loop; i++){
             weeklyData.push(null);
         }
 
@@ -30,59 +95,13 @@ exports.makeUserData = async function func(userId){
             thisWeekGoal:null,
             todayGoal:null,
             weeklyData:weeklyData,
-            monthlyData:[0,0,0],
+            monthlyData:[ZERO,ZERO,ZERO],
             lastUpdate:date
         }
 
         await db.insert("main","user",newData);
     }
-}
-
-/**
- * userIdから情報を取得し、相対的な日付のデータを修正して返す&更新する関数
- * @param userId 取得するユーザーID
- * @param userId
- * @returns {Promise<WithId<Document>>}
- */
-exports.getUser = async function func(userId){
-    const user = (await db.find("main","user",{userId:userId}))[0];
-    const date = new Date();
-    const nextMonday = new Date();
-    nextMonday.setDate(user.lastUpdate.getDate() - user.lastUpdate.getDay() + 7 + 1);
-
-    if(date.toFormat("YYYYMMDD") !== user.lastUpdate.toFormat("YYYYMMDD")) {　//同じ日
-        if (nextMonday < date) { //同じ週
-            for (let i = 0; i < date.getDay() + 1; i++) {
-                if (user.weeklyData[i] === null) user.weeklyData[i] = 0;
-            }
-        }
-        else { //異なる週
-            let i=2;
-            for(; i < 2+3+1; i++){
-                nextMonday.setDate(user.lastUpdate.getDate() - user.lastUpdate.getDay() + 7*i + 1);
-                if (nextMonday > date)break;
-            }
-
-            const weekSum = user.weeklyData.reduce((sum, element) => sum + element, 0);
-            switch(i){
-                case 2:
-                    user.weeklyData = [weekSum,user.weeklyData[0],user.weeklyData[1]];
-                    break;
-                case 3:
-                    user.weeklyData = [0,weekSum,user.weeklyData[0]];
-                    break;
-                case 4:
-                    user.weeklyData = [0,0,weekSum];
-                    break;
-                default:
-                    user.weeklyData = [0,0,0];
-                    break;
-            }
-            user.weeklyData=[0,0,0,0,0,0,0];
-        }
-        user.lastUpdate = date;
-        await db.update("main","user",{"userId":userId},{$set:user});
+    else{
+        await userData.getUser(userId);
     }
-    delete user._id;
-    return user;
 }
