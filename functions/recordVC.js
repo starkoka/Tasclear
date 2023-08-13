556756ff81fb989ecda7424b0e46155891a9af17
starkoka <j22437@inc.kisarazu.ac.jp>
2023-08-13 23:52:44 +0900

    日付をまたがないときの処理を実装

--- functions/recordVC.js
+++ functions/recordVC.js
@@ -3,6 +3,8 @@ const system = require('./logsystem.js');
 const db = require('./db.js');
 const userData = require('./userData.js');
 
+const secondsOfOneDay = 86400;
+
 async function joinVC(newState){
     await userData.makeUserData(newState.id);
     const date = new Date();
@@ -15,7 +17,31 @@ async function joinVC(newState){
 }
 
 async function leaveVC(oldState){
-    const user  = userData.getUser(oldState.id);
+    const now = new Date();
+    let user  = await userData.getUser(oldState.id);
+    user.isJoined = false;
+
+    const secondDay = new Date(user.joinedAt); //参加した日の次の0時を取得
+    secondDay.setDate(secondDay.getDate() + 1);
+    secondDay.setHours(0, 0, 0);
+
+    if(now < secondDay){ //日付をまたがない場合
+        const day = (now.getDay() === 0 ? 6 : now.getDay()-1);
+        user.weeklyData[day] += (now-user.joinedAt) / 1000;
+    }
+    else if(now === secondDay){ //万が一ミリ秒単位で真夜中だったら
+        user  = await userData.getUser(oldState.id);
+        if(now.getDay() === 1){
+            user.monthlyData[0][6] += (now-user.joinedAt) / 1000;
+        }
+        else{
+            const day = (now.getDay() === 0 ? 5 : now.getDay()-2);
+            user.weeklyData[day] += (now-user.joinedAt) / 1000;
+        }
+    }
+    else{
+
+    }
 }
 
 exports.vcStateUpdate = async function func(oldState, newState) {
