70509b1e19d3abf8a088c439eeeacfcfb9871ede
starkoka <j22437@inc.kisarazu.ac.jp>
2023-08-16 23:13:22 +0900

    コラッツ予想を解いてこのbotのサーバー代を稼ぎたい話(ギルド入退出は完成、ユーザー入退出の動作チェックまだ)

--- functions/guildData.js
+++ functions/guildData.js
@@ -1,12 +1,18 @@
 const db = require('./db.js');
 
 exports.joinGuild = async function(interaction){
-    const guild = client.guilds.cache.get(interaction.guildId) ?? await client.guilds.fetch(interaction.guildId);
+    const guild = client.guilds.cache.get(interaction.id) ?? await client.guilds.fetch(interaction.id);
+    await db.insert("main","guild",{guildId:interaction.id,studyRole:null,deleteRole:[]});
+
+    const guildMember = await guild.members.fetch()
+    for(let i=0; i < guild.memberCount;i++){
+        if(!guildMember.at(i).user.bot) await db.insert("main","guildUser",{userId:guildMember.at(i).user.id,guildId:interaction.id});
+    }
 }
 
 exports.deleteGuild = async function(interaction){
-    await db.delete("main","guild",{guildId:interaction.guildId});
-    await db.delete("main","guildUser",{guildId:interaction.guildId});
+    await db.delete("main","guild",{guildId:interaction.id});
+    await db.delete("main","guildUser",{guildId:interaction.id});
 }
 
 exports.joinMember = async function(interaction){
