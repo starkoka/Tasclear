ed221a6dfe7eb41ac45be61abb2349e2dd5b3477
yukipeti <ugachan0817@gmail.com>
2023-08-17 04:40:37 +0900

    ぐっじょぶ

--- commands/goal-set.js
+++ commands/goal-set.js
@@ -23,23 +23,18 @@ module.exports = [
             )
             .addIntegerOption((option) => option.setName("時間").setDescription("時間を設定します").setRequired(true)),
 
-        /* 一応おいて置くの細道
-            .addIntegerOption(option => option.setName("今日").setDescription("今日の目標時間を設定します").setRequired(false))
-            .addIntegerOption(option => option.setName("今週").setDescription("今週の目標時間を設定します").setRequired(false))
-            .addIntegerOption(option => option.setName("毎日").setDescription("毎日の目標時間を設定します").setRequired(false))
-            .addIntegerOption(option => option.setName("毎週").setDescription("毎週の目標時間を設定します").setRequired(false)),
-            */
-
         async execute(interaction) {
             // 入力されたoptionの代入
             const goal = interaction.options.getString("対象");
 
             const userID = interaction.user.id;
-            console.log(goal);
 
             switch (goal) {
                 case "0": {
-                    const goalToday = interaction.options.getInteger("時間");
+                    let goalToday = interaction.options.getInteger("時間");
+                    if (goalToday === 0) {
+                        goalToday = null;
+                    }
                     await db.update(
                         "main",
                         "user",
@@ -53,7 +48,10 @@ module.exports = [
                     break;
                 }
                 case "1": {
-                    const goalThisWeek = interaction.options.getInteger("時間");
+                    let goalThisWeek = interaction.options.getInteger("時間");
+                    if (goalThisWeek === 0) {
+                        goalThisWeek = null;
+                    }
                     await db.update(
                         "main",
                         "user",
@@ -67,7 +65,10 @@ module.exports = [
                     break;
                 }
                 case "2": {
-                    const goalEveryDay = interaction.options.getInteger("時間");
+                    let goalEveryDay = interaction.options.getInteger("時間");
+                    if (goalEveryDay === 0) {
+                        goalEveryDay = null;
+                    }
                     await db.update(
                         "main",
                         "user",
@@ -81,7 +82,10 @@ module.exports = [
                     break;
                 }
                 case "3": {
-                    const goalEveryWeek = interaction.options.getInteger("時間");
+                    let goalEveryWeek = interaction.options.getInteger("時間");
+                    if (goalEveryWeek === 0) {
+                        goalEveryWeek = null;
+                    }
                     await db.update(
                         "main",
                         "user",
